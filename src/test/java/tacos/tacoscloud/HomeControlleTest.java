5a0b0e92e05edbef0f8b5650a36d42c4034edd79
Маклашев Артем <maklashev_ak@outlook.com>
2023-07-19 21:22:07 +0400

    initial commit

---
+++ src/test/java/tacos/tacoscloud/HomeControlleTest.java
@@ -0,0 +1,28 @@
+package tacos.tacoscloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+// import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.hamcrest.Matchers.containsString;
+
+@WebMvcTest(HomeController.class)
+public class HomeControlleTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.view().name("home"))
+                .andExpect(MockMvcResultMatchers.content().string(containsString("Welcome to...")));
+           
+    }
+
+    
+}
